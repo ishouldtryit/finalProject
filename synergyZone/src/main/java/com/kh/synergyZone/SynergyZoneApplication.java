d14ff8cbd9dfcd974d0f82e83380ae8a7fd586f2
ishouldtryit <ishouldtryit@naver.com>
2023-05-04 10:21:08 +0900

    최초 세팅

---
+++ synergyZone/src/main/java/com/kh/synergyZone/SynergyZoneApplication.java
@@ -0,0 +1,13 @@
+package com.kh.synergyZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynergyZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynergyZoneApplication.class, args);
+	}
+
+}

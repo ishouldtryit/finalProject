f146a9b15a96cd0c5f8560df4248353b7d2e0868
HOON0904 <hu185@naver.com>
2023-06-12 20:02:20 +0900

    404 완성

---
+++ synergyZone/src/main/java/com/kh/synergyZone/advice/ErrorController.java
@@ -0,0 +1,30 @@
+package com.kh.synergyZone.advice;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class ErrorController {
+
+	@ExceptionHandler(Exception.class)
+	public String error(Exception ex) {
+		return "/error/404";
+	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public String notFound(Exception ex) {
+		ex.printStackTrace();
+		return "/error/404";
+	}
+	
+	@ExceptionHandler(RequirePermissionException.class)
+	public String forbidden(Exception ex) {
+		return "/error/404";
+	}
+	
+	@ExceptionHandler(RequireLoginException.class)
+	public String unAuthorized(Exception ex) {
+		return "redirect:/login";
+	}
+	
+}
